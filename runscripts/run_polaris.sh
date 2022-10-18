a5e4b411ff4a33441d5a71f184408f1f4f8b6b07
linked-liszt <anoxymoren@gmail.com>
2022-10-18 17:57:40 +0000

    Add polaris runscripts

---
+++ runscripts/run_polaris.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+#PBS -l select=1:system=polaris
+#PBS -l place=scatter
+#PBS -l walltime=0:30:00
+#PBS -l filesystems=home:grand
+#PBS -q debug
+#PBS -A APSDataAnalysis
+
+# Change to working directory
+cd ${PBS_O_WORKDIR}
+
+# MPI and OpenMP settings
+NNODES=`wc -l < $PBS_NODEFILE`
+NRANKS_PER_NODE=32
+NDEPTH=2
+NTHREADS=2
+
+NTOTRANKS=$(( NNODES * NRANKS_PER_NODE ))
+echo "NUM_OF_NODES= ${NNODES} TOTAL_NUM_RANKS= ${NTOTRANKS} RANKS_PER_NODE= ${NRANKS_PER_NODE} THREADS_PER_RANK= ${NTHREADS}"
+
+mpiexec -n ${NTOTRANKS} --ppn ${NRANKS_PER_NODE} --depth=${NDEPTH} --cpu-bind depth --env OMP_NUM_THREADS=${NTHREADS} -env OMP_PLACES=threads \
+    /eagle/projects/APSDataAnalysis/mprince/lau_env_polaris/bin/python \
+    /eagle/projects/APSDataAnalysis/mprince/lau/laue-parallel/script-main.py \
+    /eagle/projects/APSDataAnalysis/mprince/lau/laue-parallel/configs/config-64.yml \
+    --log_time 
