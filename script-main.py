222bd24f8a141d288547de2b204f62d5e9b199b4
linked-liszt <anoxymoren@gmail.com>
2022-10-18 18:31:56 +0000

    Add h5 parallel and additional safety

--- script-main.py
+++ script-main.py
@@ -23,7 +23,7 @@ def time_wrap(func, time_data, time_key):
     return wrap
 
 
-def main(path, dry_run=False, debug=False, log_time=False):
+def main(path, dry_run=False, debug=False, log_time=False, h5_backup=False):
     """Runs the reconstruction workflow given parameters 
     in a configuration file.
 
@@ -53,6 +53,12 @@ def main(path, dry_run=False, debug=False, log_time=False):
         if rank == 0:
             if not os.path.exists(time_dir):
                 os.mkdir(time_dir)
+    
+    if h5_backup:
+        h5_backup_dir = os.path.join(file['output'], 'h5_backup')
+        if rank == 0:
+            if not os.path.exists(h5_backup_dir):
+                os.mkdir(h5_backup_dir)
     
     no = comp['scannumber']
     gr = comp['gridsize']
@@ -96,23 +102,25 @@ def main(path, dry_run=False, debug=False, log_time=False):
         cold.resolve = time_wrap(cold.resolve, time_data, 'cold_resolve')
     dep, lau = cold.resolve(data, ind, pos, sig, geo, comp)
     
-    #cold.saveimg(file['output'] + '/pos-' + str(rank) + '/pos', pos, ind, (2048, 2048), file['frame'])
-    #cold.saveimg(file['output'] + '/lau-' + str(m) + '/lau', lau, ind, (2048, 2048), frame0, swap=True)
-
-    print (rank, lau.shape, ind.shape, pos.shape, sig.shape, dep.shape, frame0, file['frame'])
-
-    # the three lines below are for parallel hdf5
-
-    # this is not tested code, I had it once and tested, but deleted by mistake. Trying to retrieve it. It's saving
-    # in npy, we would need to save it as hd5
+    print(rank, lau.shape, ind.shape, pos.shape, sig.shape, dep.shape, frame0, file['frame'])
 
     if log_time:
         write_start_time = datetime.datetime.now()
 
     if comp['h5parallel']:
-        f = h5py.File(file['output'] + 'out' + str(scanpoint) + '.hdf5', 'w', driver='mpio', comm=comm)
-        dset = f.create_dataset('lau', (lau.shape[0] * int(size/no), lau.shape[1]), dtype=float)
-        dset[pointer * lau.shape[0] : (pointer + 1) * lau.shape[0], :] = lau
+        print(f'h5 write: {rank}, {pointer * lau.shape[0]}, {(pointer + 1) * lau.shape[0]}, {lau.shape}')
+        if h5_backup:
+            with h5py.File(os.path.join(h5_backup_dir, f'{scanpoint}_{pointer}.hd5'), 'w') as hf:
+                hf.create_dataset('pos', data=pos)
+                hf.create_dataset('lau', data=lau)
+        scan_comm.Barrier()
+        with h5py.File(os.path.join(file['output'], f'out{scanpoint}.hdf5'), 'w', driver='mpio', comm=scan_comm) as h5_f:
+            dset_lau = h5_f.create_dataset('lau', (lau.shape[0] * int(size/no), lau.shape[1]), dtype=float)
+            dset_lau[pointer * lau.shape[0] : (pointer + 1) * lau.shape[0], :] = lau
+            scan_comm.Barrier()
+            dset_pos = h5_f.create_dataset('pos', (pos.shape[0] * int(size/no),),  dtype=float)
+            dset_pos[pointer * pos.shape[0] : (pointer + 1) * pos.shape[0]] = pos
+
 
     else:
         reduced = scan_comm.gather([scanpoint, ind, lau, pos, sig, dep], root=0)
@@ -132,18 +140,13 @@ def main(path, dry_run=False, debug=False, log_time=False):
             lau = np.concatenate(lau)
             pos = np.concatenate(pos)
 
-            if not os.path.exists(file['output'] + '/pos-' + str(scanpoint)):
-                os.mkdir(file['output'] + '/pos-' + str(scanpoint))
-
-            if not os.path.exists(file['output'] + '/lau-' + str(scanpoint)):
-                os.mkdir(file['output'] + '/lau-' + str(scanpoint))
-
             with h5py.File(file['output'] +'/'+str(scanpoint) + '.hd5', 'w') as hf:
                 hf.create_dataset('pos', data=pos)
                 hf.create_dataset('lau',data=lau)
 
     if log_time:
         time_data['write_time'] = (datetime.datetime.now() - write_start_time).total_seconds()
+        time_data['walltime'] = (datetime.datetime.now() - setup_start_time).total_seconds()
         with open(os.path.join(time_dir, f'proc_{rank}.json'), 'w') as time_f:
             json.dump(time_data, time_f)
 
