10568eb7be8a88ab8da19356df7ab66586fb41bc
linked-liszt <anoxymoren@gmail.com>
2022-11-01 18:13:42 +0000

    Update theta scripts with new launch settings

--- runscripts/queue_theta.sh
+++ runscripts/queue_theta.sh
@@ -1,1 +1,2 @@
-qsub -n 2 -t 60 -A APSDataAnalysis -q debug-flat-quad runscripts/run_theta.sh
+qsub -n 2 -t 60 -A APSDataAnalysis -q debug-cache-quad --attrs filesystems=home,eagle \
+    /eagle/APSDataAnalysis/mprince/lau/laue-parallel/runscripts/run_theta.sh
--- runscripts/run_theta.sh
+++ runscripts/run_theta.sh
@@ -1,2 +1,20 @@
-cd /projects/APSDataAnalysis/mprince/lau/lau-holding
-aprun -n 64 -N 32 /projects/APSDataAnalysis/mprince/lau/env/lau_env/bin/python laue_parallel.py configs/config-64.yml --log_time
+cd /eagle/APSDataAnalysis/mprince/lau/laue-parallel
+
+CONFIGPATH=/eagle/projects/APSDataAnalysis/mprince/lau/laue-parallel/configs/config-64.yml
+
+echo ${CONFIGPATH}
+
+aprun -n 64 -N 32 -d 2 -j 1 \
+    /eagle/APSDataAnalysis/mprince/lau/env_theta/lau_theta/bin/python \
+    /eagle/projects/APSDataAnalysis/mprince/lau/laue-parallel/laue_parallel.py \
+    ${CONFIGPATH} \
+    --log_time \
+    --h5_backup \
+    --disable_recon
+
+echo "Completed processing..."
+
+aprun -n 64 -N 32 -d 2 -j 1 \
+    /eagle/APSDataAnalysis/mprince/lau/env_theta/lau_theta/bin/python \
+    /eagle/projects/APSDataAnalysis/mprince/lau/laue-parallel/recon_parallel.py \
+    ${CONFIGPATH} \
