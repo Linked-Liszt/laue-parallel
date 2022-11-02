bb267d4a8f108c3b50f770fb4204bb13b8cb4617
linked-liszt <anoxymoren@gmail.com>
2022-11-02 16:39:12 +0000

    Add preliminary theta tests for env switch

--- runscripts/queue_theta.sh
+++ runscripts/queue_theta.sh
@@ -1,2 +1,2 @@
-qsub -n 2 -t 60 -A APSDataAnalysis -q debug-cache-quad --attrs filesystems=home,eagle \
+qsub -n 1 -t 60 -A APSDataAnalysis -q debug-cache-quad --attrs filesystems=home,eagle \
     /eagle/APSDataAnalysis/mprince/lau/laue-parallel/runscripts/run_theta.sh
--- runscripts/run_theta.sh
+++ runscripts/run_theta.sh
@@ -4,7 +4,7 @@ CONFIGPATH=/eagle/projects/APSDataAnalysis/mprince/lau/laue-parallel/configs/con
 
 echo ${CONFIGPATH}
 
-aprun -n 64 -N 32 -d 2 -j 1 \
+aprun -n 64 -N 64 -d 1 -j 1 -cc depth \
     /eagle/APSDataAnalysis/mprince/lau/env_theta/lau_theta/bin/python \
     /eagle/projects/APSDataAnalysis/mprince/lau/laue-parallel/laue_parallel.py \
     ${CONFIGPATH} \
@@ -14,7 +14,7 @@ aprun -n 64 -N 32 -d 2 -j 1 \
 
 echo "Completed processing..."
 
-aprun -n 64 -N 32 -d 2 -j 1 \
+aprun -n 64 -N 64 -d 1 -j 1 -cc depth \
     /eagle/APSDataAnalysis/mprince/lau/env_theta/lau_theta/bin/python \
     /eagle/projects/APSDataAnalysis/mprince/lau/laue-parallel/recon_parallel.py \
     ${CONFIGPATH} \
