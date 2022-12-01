2125fcf6260a257f91d44f9a98aee04e443bd903
linked-liszt <anoxymoren@gmail.com>
2022-12-01 21:33:34 +0000

    Fix affinity and theta scripts

--- runscripts/al/queue_theta.sh
+++ runscripts/al/queue_theta.sh
@@ -1,2 +1,2 @@
 qsub -n 1 -t 60 -A APSDataAnalysis -q debug-cache-quad --attrs filesystems=home,eagle \
-    /eagle/APSDataAnalysis/mprince/lau/dev/laue-parallel/runscripts/run_theta.sh
+    /eagle/APSDataAnalysis/mprince/lau/dev/laue-parallel/runscripts/al/run_theta.sh
