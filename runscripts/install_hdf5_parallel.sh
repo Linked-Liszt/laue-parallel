0ec04349d343ed9bf2ed469fe0172bc3eba62ed4
linked-liszt <anoxymoren@gmail.com>
2022-12-20 21:38:47 +0000

    Add hdf5 install script

---
+++ runscripts/install_hdf5_parallel.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+PIP_PATH=/eagle/projects/APSDataAnalysis/mprince/lau_env_polaris/bin/pip
+
+# DOES NOT WORK WITH NVHPC
+module swap PrgEnv-nvhpc PrgEnv-gnu
+module load nvhpc-mixed
+
+module load cray-hdf5-parallel
+export HDF5_MPI="ON"
+# HDF5 uses CC, but wants C compiler (NOT C++)
+export CC=/opt/cray/pe/craype/2.7.15/bin/cc
+${PIP_PATH} uninstall --yes h5py
+${PIP_PATH} install --no-binary=h5py h5py
