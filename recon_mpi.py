33aa9062af8907a3332391fb7b07cce2e7785485
linked-liszt <anoxymoren@gmail.com>
2022-12-20 22:08:58 +0000

    Add single script launch demo & mpi recon prototype

---
+++ recon_mpi.py
@@ -0,0 +1,157 @@
+from ast import arg, parse
+from multiprocessing.sharedctypes import Value
+from turtle import back
+import h5py
+import argparse
+import os
+import numpy as np
+import math
+import cold
+import shutil
+from mpi4py import MPI
+
+DATASETS = ['lau', 'pos', 'sig', 'ind']
+IND_PATH = 'ind'
+PROC_OUT_DIR = 'proc_results'
+RECON_OUT_DIR = 'recon'
+ALL_OUTS = 'all_recons_test'
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description='Script to reconstruct output from the proc dumps in a run.'
+    )
+    parser.add_argument(
+        'config_fp',
+        help='path to the config used to create the run'
+    )
+    parser.add_argument(
+        '--p',
+        help='path override for manual running'
+    )
+    parser.add_argument(
+        '--start_im',
+        type=int,
+        help='Specify a start image through command line.'
+    )
+    return parser.parse_args()
+
+
+def reconstruct_backup(base_path, frame, im_num, all_dir, comm):
+    rank = comm.Get_rank()
+
+    dim_y = frame[1] - frame[0]
+    dim_x = frame[3] - frame[2]
+    im_dim = dim_y
+
+    if dim_y != dim_x:
+        raise NotImplementedError("Can only reconstruct square images!")
+
+    max_proc = -1
+    avail_datasets = None
+    dims = None
+    print(f'Rank {rank} processing im {im_num}')
+    if not os.path.exists(os.path.join(base_path, str(im_num))):
+        return 
+
+    out_dir = os.path.join(base_path, str(im_num), RECON_OUT_DIR)
+    backup_dir = os.path.join(base_path, str(im_num), PROC_OUT_DIR)
+
+    # Determine global data properties
+    if rank == 0:
+        if not os.path.exists(backup_dir):
+            os.makedirs(backup_dir)
+
+        if not os.path.exists(out_dir):
+            os.makedirs(out_dir)
+
+        if not os.path.exists(all_dir):
+            os.makedirs(all_dir)
+
+        for fp in os.listdir(backup_dir):
+            rank = int(fp.split('.')[0])
+            if rank > max_proc:
+                max_proc = rank
+        max_proc += 1
+        print(f'Found max rank: {max_proc}')
+
+        dims = {}
+        avail_datasets = []
+        with h5py.File(os.path.join(backup_dir, f'0.hd5'), 'r') as h5_f:
+            for ds_path in DATASETS:
+                if ds_path in h5_f:
+                    dims[ds_path] = h5_f[ds_path].shape
+                    avail_datasets.append(ds_path)
+
+    max_proc = comm.bcast(max_proc, root=0)
+    avail_datasets = comm.bcast(avail_datasets, root=0)
+    dims = comm.bcast(dims, root=0)
+    print(f'{rank} {avail_datasets} {dims}')
+
+    print('Constructing ind')
+    num_splits = int(math.sqrt(max_proc))
+    print(f'Calculated {num_splits} splits')
+
+    assert (im_dim % num_splits) == 0
+    
+    print('Placing proc data')
+    with h5py.File(os.path.join(out_dir, f'recon_{im_num}.hd5'), 'w', driver='mpio', comm=MPI.COMM_WORLD) as h5_f_out:
+        h5_dsets = {}
+        for ds_path in avail_datasets:
+            if len(dims[ds_path]) == 1:
+                h5_dsets[ds_path] = h5_f_out.create_dataset(ds_path, (im_dim, im_dim))
+
+            elif len(dims[ds_path]) == 2:
+                h5_dsets[ds_path] = h5_f_out.create_dataset(ds_path, (im_dim, im_dim, dims[ds_path][1]))
+
+        for i in range(max_proc):
+            with h5py.File(os.path.join(all_dir, f'{i}.hd5'), 'r') as h5_f_in:
+                proc_ind = np.array(h5_f_in[IND_PATH])
+                proc_ind[:,0] -= frame[0]
+                proc_ind[:,1] -= frame[2]
+                for ds_path in avail_datasets:
+                    ds = np.array(h5_f_in[ds_path])
+                    for j, ind in enumerate(proc_ind):
+                        if len(dims[ds_path]) == 1:
+                            h5_dsets[ds_path][ind[0], ind[1]] = ds[j]
+
+                        elif len(dims[ds_path]) == 2:
+                            h5_dsets[ds_path][:, ind[0], ind[1]] = ds[j]
+                            
+
+def recon_from_config(comm, config_fp, override_start=None):
+    mpi_rank = comm.Get_rank()
+    conf_file, conf_comp, conf_geo, conf_algo = cold.config(config_fp)
+
+
+    base_path = conf_file['output']
+    all_outs = os.path.join(conf_file['output'], ALL_OUTS)
+
+    if mpi_rank == 0:
+        if not os.path.exists(all_outs):
+            os.makedirs(all_outs)
+    comm.Barrier()
+
+    im_num = conf_comp['scanstart']
+    if override_start is not None:
+        im_num = override_start
+
+    if mpi_rank < conf_comp['scannumber']:
+        reconstruct_backup(base_path, 
+                           conf_file['frame'],
+                           im_num,
+                           all_outs,
+                           comm)
+
+
+if __name__ == '__main__':
+    args = parse_args()
+    comm = MPI.COMM_WORLD
+    try:
+        recon_from_config(comm, args.config_fp, args.start_im)
+    except Exception as e:
+        import traceback
+        with open('err_recon.log', 'a+') as err_f:
+            err_f.write(str(e) + '\n') # MPI term output can break.
+            err_f.write('Traceback: \n')
+            err_f.write(traceback.format_exc())
+        comm.Abort(1) # Term run early to prevent hang.
---
+++ runscripts/debug/queue_polaris_recon.sh
@@ -0,0 +1,35 @@
+NUM_NODES=1
+RANKS_PER_NODE=4
+START_IM=3
+PROJ_NAME=mpi_recon
+
+AFFINITY_PATH=../runscripts/set_gpu_affinity.sh
+PYTHON_PATH=/eagle/projects/APSDataAnalysis/mprince/lau_env_polaris/bin/python 
+CONFIG_PATH=../configs/KS_10UN2/prod-config-KS_10UN2_mask.yml
+
+echo "
+cd \${PBS_O_WORKDIR}
+
+# MPI and OpenMP settings
+NNODES=\`wc -l < \$PBS_NODEFILE\`
+NRANKS_PER_NODE=${RANKS_PER_NODE}
+NDEPTH=2
+NTHREADS=2
+
+NTOTRANKS=\$(( NNODES * NRANKS_PER_NODE ))
+echo \"NUM_OF_NODES= \${NNODES} TOTAL_NUM_RANKS= \${NTOTRANKS} RANKS_PER_NODE= \${NRANKS_PER_NODE} THREADS_PER_RANK= \${NTHREADS}\"
+
+mpiexec -n \${NTOTRANKS} --ppn \${NRANKS_PER_NODE} --depth=\${NDEPTH} --cpu-bind depth --env OMP_NUM_THREADS=\${NTHREADS} -env OMP_PLACES=threads \\
+    ${AFFINITY_PATH} \\
+    ${PYTHON_PATH} \\
+    ../recon_mpi.py \\
+    ${CONFIG_PATH} \\
+    --start_im ${START_IM} \\
+" | \
+qsub -A APSDataAnalysis \
+-q debug \
+-l select=${NUM_NODES}:system=polaris \
+-l walltime=0:50:00 \
+-l filesystems=home:eagle \
+-l place=scatter \
+-N ${PROJ_NAME} 
